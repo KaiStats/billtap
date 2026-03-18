8b888992074c778a3e54ee974fe9ce6bae036511
Kai Kogmon <rainmakker247@gmail.com>
2026-03-18 15:16:36 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
 const MainPage = mainPageKey ? Pages[mainPageKey] : <></>;
 
-const LayoutWrapper = ({ children, currentPageName }) => Layout ?
+const LayoutWrapper = ({ children, currentPageName }) => (Layout && typeof Layout === 'function') ?
   <Layout currentPageName={currentPageName}>{children}</Layout>
   : <>{children}</>;
 
