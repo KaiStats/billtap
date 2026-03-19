c5ba67b94a62babbbe69014fc9c5502ebdf822c0
Kai Kogmon <rainmakker247@gmail.com>
2026-03-19 01:47:08 +0000

    File changes

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import HeroSection from '@/components/HeroSection';
+import HowItWorks from '@/components/HowItWorks';
+import Features from '@/components/Features';
+import FinalCTA from '@/components/FinalCTA';
+import Footer from '@/components/Footer';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <HeroSection />
+      <HowItWorks />
+      <Features />
+      <FinalCTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
